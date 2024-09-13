bd59a8d4a07f0bb8535957dfb140212b8b7ed32f
Ali-Hossam <ali.hossamx22@gmail.com>
2024-09-14 00:05:25 +0300

    added partials folder

--- views/partials/nav_bar.view.php
+++ views/partials/nav_bar.view.php
@@ -1,4 +1,5 @@
 <ul class="nav-bar">
-  <img src="assets/pen_icon_no_bkg.png" width="50" />
-  <button id="register-button">التسجيل</button>
+  <img src="/assets/pen_icon_no_bkg.png" width="50" />
+  <button id="register-button"
+    onclick="window.location.href='/views/register.view.php'">التسجيل</button>
 </ul>
