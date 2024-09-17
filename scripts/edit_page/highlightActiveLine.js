c39c66aa4990b0bd0d77e65c0a27e4427022c767
Ali-Hossam <ali.hossamx22@gmail.com>
2024-09-18 00:02:46 +0300

    Created a javascript file for highlighting active line in the editor

---
+++ scripts/edit_page/highlightActiveLine.js
@@ -0,0 +1,51 @@
+let prevLineElement = null;
+let activeElement = null;
+
+function highlightLine() {
+  const selection = window.getSelection();
+  activeElement = selection.focusNode;
+
+  // Active element is text node when the line isn't empty <div> some text </div>
+  if (activeElement && activeElement.nodeType === Node.TEXT_NODE) {
+    activeElement = activeElement.parentElement;
+  }
+
+  // Remove highlight from the previous line
+  if (prevLineElement) {
+    prevLineElement.classList.remove("highlighted-bkg");
+  }
+
+  if (activeElement) {
+    activeElement.classList.add("highlighted-bkg");
+    prevLineElement = activeElement;
+  }
+}
+
+// function highlightLineNumber() {
+//   const selection = window.getSelection();
+//   activeElement = selection.focusNode;
+
+//   // Get the index of the active element in it's parent childs
+//   if (activeElement && activeElement.nodeType === Node.TEXT_NODE) {
+//     activeElement = activeElement.parentElement;
+//     const activeElementParent = activeElement.parentElement;
+//     const elementIdx = Array.prototype.indexOf.call(
+//       activeElementParent.children,
+//       activeElement
+//     );
+
+//     // Get the corresponding line number
+//     const linesNumbers = document.querySelectorAll(".lines-numbers-container .line-number");
+//     const activeLineNumber = linesNumbers[elementIdx - 1];
+//     activeLineNumber.classList.add("highlighted-number");
+//   }
+// }
+
+contentEditor.addEventListener("keydown", () => {
+  setTimeout(() => {
+    highlightLine();
+  }, 5);
+  
+  updateLineNumbers();
+  // highlightLineNumber();
+});
