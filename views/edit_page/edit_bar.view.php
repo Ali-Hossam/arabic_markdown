2c1f2a32f576eb0e672d39cc6a1d59a953a697c0
Ali-Hossam <ali.hossamx22@gmail.com>
2024-09-18 00:00:00 +0300

    Created a php file for the view of the edit bar in the typing page

---
+++ views/edit_page/edit_bar.view.php
@@ -0,0 +1,91 @@
+<div class="edit-bar">
+  <div class="website-logo">
+    <img src="/assets/pen_icon_no_bkg.png" alt="website-icon" />
+  </div>
+
+  <div class="edit-bar-icons-container">
+    <ul class="edit-bar-icons-subcontainer no-border">
+      <li class="edit-bar-icon dropdown">
+        <img src="/assets/edit_page_icons/font1.png" alt="Font type" />
+        <span class="dropdown-icon">&#8595;</span>
+        <span class="tooltip">نوع الخط</span>
+      </li>
+      <li class="edit-bar-icon dropdown">
+        <img src="/assets/edit_page_icons/hash.svg" alt="Font size" />
+        <span class="dropdown-icon">&#8595;</span>
+        <span class="tooltip">حجم الخط</span>
+      </li>
+    </ul>
+
+    <ul class="edit-bar-icons-subcontainer">
+      <li class="edit-bar-icon">
+        <img src="/assets/edit_page_icons/bold.svg" alt="Bold" />
+        <span class="tooltip">سميك</span>
+      </li>
+      <li class="edit-bar-icon">
+        <img src="/assets/edit_page_icons/italic.svg" alt="Italic" />
+        <span class="tooltip">مائل</span>
+      </li>
+      <li class="edit-bar-icon">
+        <img
+          src="/assets/edit_page_icons/through.svg"
+          alt="Strikethrough" />
+        <span class="tooltip">خط متوسط</span>
+      </li>
+    </ul>
+
+    <ul class="edit-bar-icons-subcontainer">
+      <li class="edit-bar-icon">
+        <img src="/assets/edit_page_icons/list.svg" alt="List" />
+        <span class="tooltip">قائمة</span>
+      </li>
+      <li class="edit-bar-icon">
+        <img
+          src="/assets/edit_page_icons/numbered_list.svg"
+          alt="Numbered List" />
+        <span class="tooltip">قائمة مرقمة</span>
+      </li>
+      <li class="edit-bar-icon">
+        <img
+          src="/assets/edit_page_icons/check_list.svg"
+          alt="Check List" />
+        <span class="tooltip">قائمة تحقق</span>
+      </li>
+    </ul>
+
+    <ul class="edit-bar-icons-subcontainer">
+      <li class="edit-bar-icon">
+        <img src="/assets/edit_page_icons/image.svg" alt="Insert Image" />
+        <span class="tooltip">صورة</span>
+      </li>
+      <li class="edit-bar-icon">
+        <img src="/assets/edit_page_icons/link.svg" alt="Add Link" />
+        <span class="tooltip">رابط</span>
+      </li>
+    </ul>
+
+    <ul class="edit-bar-icons-subcontainer">
+      <li class="edit-bar-icon">
+        <img src="/assets/edit_page_icons/code.svg" alt="Insert Code" />
+        <span class="tooltip">كود</span>
+      </li>
+      <li class="edit-bar-icon">
+        <img src="/assets/edit_page_icons/quotes.svg" alt="Quotes" />
+        <span class="tooltip">اقتباس</span>
+      </li>
+    </ul>
+  </div>
+
+  <ul class="register-icons-container">
+    <li class="edit-bar-icon">
+      <img src="/assets/edit_page_icons/mode.svg" alt="mode" />
+      <span class="tooltip">ليل/نهار</span>
+    </li>
+    <li class="edit-bar-icon">
+      <img src="/assets/edit_page_icons/export.svg" alt="Export" />
+      <span class="tooltip">تصدير</span>
+    </li>
+    <button>التسجيل</button>
+  </ul>
+
+</div>
