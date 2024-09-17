76f7450ba0c8974834da157f63670e7189341ea8
Ali-Hossam <ali.hossamx22@gmail.com>
2024-09-18 00:03:40 +0300

    Created a general javascript file for edit page settings

---
+++ scripts/edit_page/editPage.js
@@ -0,0 +1,16 @@
+// Add a div to write the content of the first line into instead of writing
+// directly in the content editor element text.
+function addFirstLine() {
+  const div = createDivElement();
+  contentEditor.appendChild(div);
+  Cursor.insertInto(div);
+}
+
+addFirstLine();
+highlightLine();
+
+contentEditor.addEventListener("keydown", (e) => {
+  if(contentEditor.childElementCount === 0) {
+    addFirstLine();
+  }
+});
