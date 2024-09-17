6f8afe9976e8c3c956911e56bfaf3fa6085bb645
Ali-Hossam <ali.hossamx22@gmail.com>
2024-09-18 00:01:49 +0300

    Created a java script file for pasting text inside the editor section as text not as html.

---
+++ scripts/edit_page/pasteText.js
@@ -0,0 +1,28 @@
+// Remove styling from pasted text
+contentEditor.addEventListener("paste", (e) => {
+  e.preventDefault();
+
+  let pastedData = e.clipboardData.getData("text/plain").trim();
+  let paragraphs = pastedData.split(/\n/);
+
+  // Create a div for each paragraph
+  paragraphs.forEach((paragraph) => {
+    const paragraphDiv = document.createElement("div");
+
+    // If there is a new line in the text it appears as empty string
+    if (!paragraph) {
+      paragraphDiv.innerHTML = "<br>";
+    } else {
+      paragraphDiv.textContent = paragraph;
+    }
+
+    contentEditor.appendChild(paragraphDiv);
+  });
+
+  Cursor.moveVertical(contentEditor, false);
+  updateLineNumbers();
+  highlightLine();
+});
+
+// Reposition Caret at the end of paste, and handle single br at the start of the div
+// and line number highlight and arabic numbers
