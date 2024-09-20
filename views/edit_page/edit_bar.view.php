dfa5e9caa2245b05b90e47f9adf04ca8c8d69a5b
Ali-Hossam <ali.hossamx22@gmail.com>
2024-09-20 22:51:29 +0300

    added a class for list elements

--- views/edit_page/edit_bar.view.php
+++ views/edit_page/edit_bar.view.php
@@ -5,28 +5,28 @@
 
   <div class="edit-bar-icons-container">
     <ul class="edit-bar-icons-subcontainer no-border">
-      <li class="edit-bar-icon dropdown">
+      <li class="list-element edit-bar-icon dropdown">
         <img src="/assets/edit_page_icons/font1.png" alt="Font type" />
-        <span class="dropdown-icon">&#8595;</span>
+        <span class="dropdown-icon">&#9660;</span>
         <span class="tooltip">نوع الخط</span>
       </li>
-      <li class="edit-bar-icon dropdown">
+      <li class="list-element edit-bar-icon dropdown">
         <img src="/assets/edit_page_icons/hash.svg" alt="Font size" />
-        <span class="dropdown-icon">&#8595;</span>
+        <span class="dropdown-icon">&#9660;</span>
         <span class="tooltip">حجم الخط</span>
       </li>
     </ul>
 
     <ul class="edit-bar-icons-subcontainer">
-      <li class="edit-bar-icon">
+      <li class="list-element edit-bar-icon">
         <img src="/assets/edit_page_icons/bold.svg" alt="Bold" />
         <span class="tooltip">سميك</span>
       </li>
-      <li class="edit-bar-icon">
+      <li class="list-element edit-bar-icon">
         <img src="/assets/edit_page_icons/italic.svg" alt="Italic" />
         <span class="tooltip">مائل</span>
       </li>
-      <li class="edit-bar-icon">
+      <li class="list-element edit-bar-icon">
         <img
           src="/assets/edit_page_icons/through.svg"
           alt="Strikethrough" />
@@ -35,17 +35,17 @@
     </ul>
 
     <ul class="edit-bar-icons-subcontainer">
-      <li class="edit-bar-icon">
+      <li class="list-element edit-bar-icon">
         <img src="/assets/edit_page_icons/list.svg" alt="List" />
         <span class="tooltip">قائمة</span>
       </li>
-      <li class="edit-bar-icon">
+      <li class="list-element edit-bar-icon">
         <img
           src="/assets/edit_page_icons/numbered_list.svg"
           alt="Numbered List" />
         <span class="tooltip">قائمة مرقمة</span>
       </li>
-      <li class="edit-bar-icon">
+      <li class="list-element edit-bar-icon">
         <img
           src="/assets/edit_page_icons/check_list.svg"
           alt="Check List" />
@@ -54,22 +54,22 @@
     </ul>
 
     <ul class="edit-bar-icons-subcontainer">
-      <li class="edit-bar-icon">
+      <li class="list-element edit-bar-icon">
         <img src="/assets/edit_page_icons/image.svg" alt="Insert Image" />
         <span class="tooltip">صورة</span>
       </li>
-      <li class="edit-bar-icon">
+      <li class="list-element edit-bar-icon">
         <img src="/assets/edit_page_icons/link.svg" alt="Add Link" />
         <span class="tooltip">رابط</span>
       </li>
     </ul>
 
     <ul class="edit-bar-icons-subcontainer">
-      <li class="edit-bar-icon">
+      <li class="list-element edit-bar-icon">
         <img src="/assets/edit_page_icons/code.svg" alt="Insert Code" />
         <span class="tooltip">كود</span>
       </li>
-      <li class="edit-bar-icon">
+      <li class="list-element edit-bar-icon">
         <img src="/assets/edit_page_icons/quotes.svg" alt="Quotes" />
         <span class="tooltip">اقتباس</span>
       </li>
@@ -77,11 +77,11 @@
   </div>
 
   <ul class="register-icons-container">
-    <li class="edit-bar-icon">
+    <li class="list-element edit-bar-icon">
       <img src="/assets/edit_page_icons/mode.svg" alt="mode" />
       <span class="tooltip">ليل/نهار</span>
     </li>
-    <li class="edit-bar-icon">
+    <li class="list-element edit-bar-icon">
       <img src="/assets/edit_page_icons/export.svg" alt="Export" />
       <span class="tooltip">تصدير</span>
     </li>
