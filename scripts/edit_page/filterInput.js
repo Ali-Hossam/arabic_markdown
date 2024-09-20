a139a6f3243e590018c80e0e99c1a34697d99f2b
Ali-Hossam <ali.hossamx22@gmail.com>
2024-09-20 22:48:04 +0300

    used execCommand in numbers conversion to arabic

--- scripts/edit_page/filterInput.js
+++ scripts/edit_page/filterInput.js
@@ -1,11 +1,7 @@
 // Convert digits in the input text to arabic
-contentEditor.addEventListener("input", (event) => {
+editor.addEventListener("beforeinput", (event) => {
   if (event.data != null && event.data >= "0" && event.data <= "9") {
-    const editableDiv = event.target;
-    const text = editableDiv.innerText;
-
-    const cursorPosition = Cursor.getPosition();
-    editableDiv.innerText = digitsToArabic(text);
-    Cursor.setPosition(editableDiv, cursorPosition);
+    event.preventDefault();
+    document.execCommand("insertText", false, digitsToArabic(event.data));
   }
 });
