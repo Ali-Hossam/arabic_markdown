e3e685ae6ce87906f90799f7ac9f7df722ac9c10
Ali-Hossam <ali.hossamx22@gmail.com>
2024-09-25 15:31:15 +0300

    replaced the function that parse empty lines with a tag !! that makes a new line

--- scripts/edit_page/utils/parseMd.js
+++ scripts/edit_page/utils/parseMd.js
@@ -15,14 +15,10 @@ function parseMd() {
   let mdText = editor.innerText;
   console.log(mdText.replace(/\n/g, "[NEWLINE]"));
 
-  // // parse empty lines after header
-  // mdText = mdText.replace(/^#.*\n\n/gm, match => match + '<br>');
-
   // replace mutliple \n with multiple $nbsp; <br> the same number of \n
-  mdText = mdText.replace(/\n\n/g, "<br>");
+  mdText = mdText.replace(/!!/g, "<br>");
 
   let parsedText = marked.parse(mdText);
-
   previewer.innerHTML = parsedText;
 }
 
