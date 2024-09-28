058f8df98c0070814cffedf3b5122c6ee508f5c1
Ali-Hossam <ali.hossamx22@gmail.com>
2024-09-29 00:10:00 +0300

    Updated available fonts

--- scripts/edit_page/toolbar/dropdown/init.js
+++ scripts/edit_page/toolbar/dropdown/init.js
@@ -1,5 +1,12 @@
-const fontsAr = ["أميري", "رقعة", "مرحي", "بالوبهيجان", "خط 3"];
-const fontsEn = ["Amiri", "rakkas", "Marhey", "Baloo Bhaijaan 2", "Readex Pro"];
+const fontsAr = ["أميري", "رقعة", "عثماني", "بالو", "زين", "لطيف"];
+const fontsEn = [
+  "Amiri",
+  "rakkas",
+  "Noto Naskh Arabic",
+  "Baloo Bhaijaan 2",
+  "zain",
+  "Lateef",
+];
 
 // Fonts dropdown menu
 fontsAr.forEach((font, index) => {
@@ -14,7 +21,7 @@ let fontSize = 20;
 for (let index = 0; index < 6; index++) {
   const element = document.createElement("li");
   element.textContent = `حجم ${index + 1}`;
-  element.style.fontSize = `${fontSize}px`; 
+  element.style.fontSize = `${fontSize}px`;
   element.style.fontFamily = "Amiri";
 
   fontSize -= 2;
