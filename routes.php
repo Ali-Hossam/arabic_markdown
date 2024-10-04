60e03a0d690afb0775bd4d9e966ab6b9c3708c90
Ali-Hossam <ali.hossamx22@gmail.com>
2024-10-04 08:02:04 +0300

    add routes to router class instance

---
+++ routes.php
@@ -0,0 +1,13 @@
+<?php
+require_once base_path("Core/Router.php");
+
+$router = new Router();
+$router->get('/', 'views/home.view.php');
+$router->get('/register', 'views/register.view.php');
+$router->post('/register', 'Controllers/Users/create.php');
+
+
+$router->get('/login', 'views/login.view.php');
+$router->post('/login', 'Controllers/Users/show.php');
+
+$router->post('/reset-password', 'Controllers/Users/resetPass.php');
