2fb557adfbfd724fd4348de715eb699bba0f738e
Ali-Hossam <ali.hossamx22@gmail.com>
2024-10-04 08:09:29 +0300

    added a config file that has global used variables like database name, main collections names, etc

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'appdb');
+define('DATA_COLLECTION', 'users'); // data collection in database
+define('RPASS_COLLECTION', 'password-resets'); // pass reset collection in database
