469a146650830f25e5bef80c8cf8c32102dad52a
Ali-Hossam <ali.hossamx22@gmail.com>
2024-10-04 08:06:58 +0300

    added form error message section

--- views/login.view.php
+++ views/login.view.php
@@ -2,18 +2,32 @@
 require("partials/head.php") ?>
 
 <body class="register-page">
-  <form action="" class="register-form">
+  <form action="/login" method="POST" class="register-form">
     <img src="/assets/pen_icon_no_bkg.png" class="web_logo" width="140px">
 
     <div class="form-group">
       <label for="email">البريد الإلكتروني</label>
-      <input type="email" id="email" name="username" placeholder="Username@Email.com" required>
+      <input type="email" id="email" name="email" placeholder="Username@Email.com" required>
     </div>
 
     <div class="form-group">
       <label for="password">كلمة السر</label>
-      <input type="password" id="password" name="password" placeholder="**************">
+      <div class="password-container">
+        <input type="password" id="password" name="password" placeholder="**************">
+        <img src="/assets/eye.svg" alt="show-password" class="show-password-icon">
+      </div>
     </div>
+
+    <p class="form-error-msg">
+      <?php
+      // Class doesn't exit if the page is loaded from <a href="link"> tag
+      if (class_exists("ErrorsManager")) {
+        if (ErrorsManager::has('login')) {
+          echo implode("<br>", ErrorsManager::get('login'));
+        }
+      }
+      ?>
+    </p>
 
     <button class="register-button">تسجيل الدخول</button>
     <div class="register-options">
@@ -22,6 +36,10 @@ require("partials/head.php") ?>
     </div>
 
   </form>
+  <script src="/scripts/Core/functions.js"></script>
+  <script>
+    loadLib("register_page/form.js");
+  </script>
 </body>
 
 
--- views/register.view.php
+++ views/register.view.php
@@ -1,7 +1,7 @@
 <?php require("partials/head.php") ?>
 
 <body class="register-page">
-  <form action="" class="register-form">
+  <form action="/register" method="POST" class="register-form">
     <img src="/assets/pen_icon_no_bkg.png" class="web_logo" width="140px">
 
     <div class="form-group">
@@ -11,18 +11,35 @@
 
     <div class="form-group">
       <label for="email">البريد الإلكتروني</label>
-      <input type="email" id="email" name="username" placeholder="Username@Email.com" required>
+      <input type="email" id="email" name="email" placeholder="Username@Email.com" required>
     </div>
-    
+
     <div class="form-group">
       <label for="password">كلمة السر</label>
-      <input type="password" id="password" name="password" placeholder="**************">
+      <div class="password-container">
+        <input type="password" id="password" name="password" placeholder="**************">
+        <img src="/assets/eye.svg" alt="show-password" class="show-password-icon">
+      </div>
     </div>
-    
+
     <div class="form-group">
-      <label for="confirm_password">كلمة السر مرة أخرى</label>
-      <input type="password" id="confirm_password" name="confirm_password" placeholder="**************" required>
+      <label for="confirm_password">تأكيد كلمة السر</label>
+      <div class="password-container">
+        <input type="password" id="confirm_password" name="confirm_password" placeholder="**************" required>
+        <img src="/assets/eye.svg" alt="show-password" class="show-password-icon">
+      </div>
     </div>
+
+    <p class="form-error-msg">
+      <?php
+      // Class doesn't exit if the page is loaded from <a href="link"> tag
+      if (class_exists("ErrorsManager")) {
+        if (ErrorsManager::has('register')) {
+          echo implode("<br>", ErrorsManager::get('register'));
+        }
+      }
+      ?>
+    </p>
 
     <button class="register-button">التسجيل</button>
     <div class="register-options">
@@ -30,6 +47,13 @@
       <a href=""></a>
     </div>
   </form>
+
+  <script src="/scripts/Core/functions.js"></script>
+  <script>
+    loadLib("register_page/form.js");
+  </script>
+
 </body>
+
 
 <?php require("partials/footer.php") ?>
