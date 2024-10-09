d5be6303996fddfa2c98b983f36a88085fb6eb29
Ali-Hossam <ali.hossamx22@gmail.com>
2024-10-09 23:54:16 +0300

    added a new variable for profile pictures (avatar) path

--- config.php
+++ config.php
@@ -3,3 +3,4 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', 'appdb');
 define('DATA_COLLECTION', 'users'); // data collection in database
 define('RPASS_COLLECTION', 'password-resets'); // pass reset collection in database
+define('PROFILE_PICS_PATH', 'assets/profiles');
