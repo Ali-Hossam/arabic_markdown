e6d16a07609eba3e92f9b18dc17e0c7cabdf2472
Ali-Hossam <ali.hossamx22@gmail.com>
2024-10-16 17:25:14 +0300

    removed debugging function

---
+++ src/index.php
@@ -0,0 +1,2 @@
+<!-- This is a fake index file used in deploying in railway so that i can 
+ build a php project then run a command to run the origina index file in public-->
--- src/public/index.php
+++ src/public/index.php
@@ -1,28 +1,6 @@
 <?php
 session_start();
-function displayFirstLevelDirectories($dir) {
-  // Open the directory
-  if ($handle = opendir($dir)) {
-      // Loop through the directory entries
-      while (false !== ($entry = readdir($handle))) {
-          // Skip the current and parent directory entries
-          if ($entry == '.' || $entry == '..') {
-              continue;
-          }
 
-          // Check if the entry is a directory
-          if (is_dir($dir . '/' . $entry)) {
-              // Print the directory name
-              echo $entry . PHP_EOL;
-          }
-      }
-      closedir($handle);
-  }
-}
-
-// Specify the directory you want to start from (parent directory)
-$startDir = __DIR__ . '/'; // Start from the parent directory
-displayFirstLevelDirectories($startDir);
 require __DIR__ . '/../Core/functions.php';
 
 require base_path("routes.php");
