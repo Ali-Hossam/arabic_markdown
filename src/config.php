8081c5961c90d63393ce4462c5159721d164fc67
Ali-Hossam <ali.hossamx22@gmail.com>
2024-10-17 16:47:13 +0300

    updated config

--- src/config.php
+++ src/config.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_HOST', 'localhost');
+define('DB_HOST', getenv('MONGOHOST') ?: 'localhost');
 define('DB_NAME', 'appdb');
 define('DATA_COLLECTION', 'users'); // data collection in database
 define('RPASS_COLLECTION', 'password-resets'); // pass reset collection in database
