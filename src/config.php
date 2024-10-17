bb29d5b6a6112c9b5660741c6c28db1905057d9e
Ali-Hossam <ali.hossamx22@gmail.com>
2024-10-17 17:35:37 +0300

    removed debug

--- src/config.php
+++ src/config.php
@@ -1,8 +1,5 @@
 <?php
-var_dump(getenv('MONGO_URL'));
 define ('MONGO_URL', getenv('MONGO_URL') ?? null);
-var_dump(MONGO_URL);
-die();
 define('DB_HOST', getenv('MONGOHOST') ?: 'localhost');
 define('DB_NAME', 'appdb');
 define('DATA_COLLECTION', 'users'); // data collection in database
